ad08a451c0564494be795ae2fa256bd1eb9febfc
pprettysimpple <andrianov.kirill2010@yandex.ru>
2019-10-30 14:59:18 +0300

    add last task

--- lab1/E.c
+++ lab1/E.c
@@ -38,23 +38,27 @@ signed main() {
             scanf("%d", in[i] + j);
             in[i][j]--;
         }
-        for (int j = (1 << sz[i]) - 1; j >= 0; j--) {
+        for (int j = 0; j < (1 << sz[i]); j++) {
             scanf("%d", in_vals[i] + j);
         }
     }
     int *ans = malloc((1 << k) * sizeof(int));
     for (int mask = 0; mask < (1 << k); mask++) {
         memset(vals, 0, n * sizeof(int));
+        int cur_mask = 0;
+        for (int i = 0; i < k; i++) {
+            cur_mask |= (((mask >> i) & 1) << (k - 1 - i));
+        }
         for (int u = 0; u < n; u++) {
             if (sz[u] <= 0) {
-                vals[u] = ((mask >> (k + sz[u] - 1)) & 1);
+                vals[u] = ((cur_mask >> (-sz[u])) & 1);
                 continue;
             }
             int res = 0;
             for (int j = 0; j < sz[u]; j++) {
                 int v = in[u][j];
                 deph[u] = max(deph[u], deph[v] + 1);
-                res |= (vals[v] << j);
+                res |= (vals[v] << (sz[u] - j - 1));
             }
             vals[u] = in_vals[u][res];
         }
